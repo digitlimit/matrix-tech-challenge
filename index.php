444dcb7576760bd8c093ecd31dd7a5813c89de53
Emeka Mbah <frankemeks77@yahoo.com>
2021-07-15 02:20:32 +0100

    validate file

--- index.php
+++ index.php
@@ -13,12 +13,15 @@
  */
 require './vendor/autoload.php';
 
+
 /**
  * Here is a simple PHP REST API to handle requests
  */
+
 // The code enables CORS - Cross-Origin Resource Sharing
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,POST");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
@@ -27,10 +30,32 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
 
-// We only have an endpoint called echo, any other should return error 404
-if ( $uri[1] !== 'echo' || isset($uri[2]) ) {
+// these are the list of available endpoints
+$endpoints = ['echo', 'invert', 'flatten', 'sum', 'multiply'];
+
+// we are only expecting one parameter, hence throw 404 for extra parameters
+// also check if the requested endpoint is available
+if( !isset($uri[1]) || !in_array($uri[1], $endpoints) || isset($uri[2]) ) {
     header("HTTP/1.1 404 Not Found");
+    echo json_encode([ 'message' => 'Not Found', 'success' => false ]);
     die;
 }
+
+// validate file before passing to controller, lets consider this block a middleware
+// for validation error status code, we will return 422 UNPROCESSABLE ENTITY
+if(!isset($_FILES['file'])){
+    header("HTTP/1.1 422 Unprocessable Entity");
+    echo json_encode([ 'message' => 'CSV file required', 'success' => false ]);
+    die;
+}
+
+// use Matrix controller to handle matrix related requests
+use App\Controllers\MatrixController;
+
+$matrixController = new MatrixController();
+//array(), array("three", "four")
+call_user_func_array([ $matrixController, $uri[1], [
+
+]]);
 
 var_dump($_POST['file']); die;
